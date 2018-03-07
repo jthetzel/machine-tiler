0b438a9b66bb2631409771095a312266de16a84f
ingalls <ingalls@protonmail.com>
2018-03-06 21:48:45 -0500

    Better cluster selection

--- cloudformation/machine-tiler.template.js
+++ cloudformation/machine-tiler.template.js
@@ -10,7 +10,7 @@ const mbp = {
         },
         Cluster: {
             Type: "String",
-            Default: "arn:aws:ecs:us-east-1:<ACCOUNT>:cluster/machine-ecs-processing-<NAME>"
+            Default: "production"
         }
     }
 };
@@ -18,7 +18,7 @@ const mbp = {
 // Generate Watchbot resources. You can use references to parameters and
 // resources that were defined above.
 const watch = watchbot.template({
-    cluster: ref('Cluster'),
+    cluster: { "Fn::Join" : [ "delimiter", [ 'arn:aws:ecs:us-east-1:', ref('AWS::AccountId'), ':cluster/machine-ecs-', ref('Cluster') ] ] }
     service: 'machine-tiler',
     serviceVersion: ref('GitSha'),
     env: { GitSha: ref('GitSha') },
